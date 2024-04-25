52a580109c0ef5c5d0d55d1b30e0e180f2f95345
AshenI99 <asheniranda99@gmail.com>
2024-04-26 01:39:09 +0530

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import {useEffect, useState} from "react";
-import Loader from "./components/Loader/Loader";
-import ErrorPopup from "./components/ErrorPopup/ErrorPopup";
 import {Navbar} from "react-bootstrap";
 
+import Loader from "./components/Loader/Loader";
+import Searchbar from "./components/Searchbar/Searchbar";
+import ErrorPopup from "./components/ErrorPopup/ErrorPopup";
+
+
+const APIKEY = "demo";
 
 const App = () => {
 
@@ -14,26 +18,27 @@ const App = () => {
     });
 
     // Loading State of the app
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
 
     // Error state of the app
     const [errorMessage, setErrorMessage] = useState("");
 
-
-    // Fetch data when the page loads
-    useEffect(() => {
-        fetchData();
-    }, [])
-
-    const fetchData= async () => {
+    const fetchData= async (stockValue) => {
         setIsLoading(true);
 
         try {
-            const income_sheet = await fetch("https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=demo");
+            const income_sheet = await fetch(`https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${stockValue}&apikey=${APIKEY}`);
             const income_data = await income_sheet.json();
 
-            const balance_sheet = await fetch("https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=IBM&apikey=demo");
+            const balance_sheet = await fetch(`https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${stockValue}&apikey=${APIKEY}`);
             const balance_data = await balance_sheet.json();
+
+            if((income_data?.Information && income_data?.Information?.includes("Our standard API rate limit is 25 requests per day")) || (balance_data?.Information && balance_data?.Information?.includes("Our standard API rate limit is 25 requests per day"))) {
+                throw new Error("Daily Limit Reached")
+
+            } else if(!income_data?.quarterlyReports || !balance_data?.quarterlyReports) {
+                throw new Error("Incorrect Stock Name")
+            }
 
             setIncomeData({
                 netIncome: income_data?.quarterlyReports?.map((el) => {
@@ -55,36 +60,45 @@ const App = () => {
                     }
                 })
             })
+
+            setErrorMessage("");
         } catch (err) {
             // Set error message
             setErrorMessage(err.message)
         } finally {
             setIsLoading(false);
         }
+    }
 
+    console.log(errorMessage)
+
+    const onSearchClickHandling = (value) => {
+        fetchData(value);
     }
 
     return (
         <div className="app-holder">
-            {isLoading ?
-                <Loader/>
-            :
-                <div className="app-content">
-                    <Navbar expand="lg" className="navbar-custom">
-                        <Navbar.Brand className="ps-3 text-white" href="#">Stock Scanner</Navbar.Brand>
-                    </Navbar>
+
+            {isLoading && <Loader/>}
+
+            <div className="app-content">
+                <Navbar expand="lg" className="navbar-custom">
+                    <Navbar.Brand className="ps-3 text-white" href="#">Stock Scanner</Navbar.Brand>
+                </Navbar>
+
+                <div className="p-4">
+                    <Searchbar onSearchHandle={onSearchClickHandling} />
 
                     {errorMessage ?
                         <>
                             <ErrorPopup message={errorMessage} />
                             <div className="error-div page-center-content text-white">{errorMessage}. Try again</div>
                         </>
-                    :
+                        :
                         ""
                     }
                 </div>
-            }
-
+            </div>
         </div>
     );
 }
